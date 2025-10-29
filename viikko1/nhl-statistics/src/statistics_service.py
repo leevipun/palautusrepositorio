39e93e3cd10e398c55db9d866ed628ee00d62aed
leevipun <leevi.puntanen2@gmail.com>
2025-10-29 16:19:50 +0200

    Enhance StatisticsService to support sorting by assists and update main function to display top players by assists

--- viikko1/nhl-statistics/src/statistics_service.py
+++ viikko1/nhl-statistics/src/statistics_service.py
@@ -1,5 +1,11 @@
 from player_reader import PlayerReader
 
+from enum import Enum
+
+class SortBy(Enum):
+    POINTS = 1
+    GOALS = 2
+    ASSISTS = 3
 
 
 class StatisticsService:
@@ -22,20 +28,40 @@ class StatisticsService:
 
         return list(players_of_team)
 
-    def top(self, how_many):
+    def top(self, how_many, criteria=SortBy.POINTS):
         # metodin käyttämä apufufunktio voidaan määritellä näin
-        def sort_by_points(player):
-            return player.points
+        if criteria == SortBy.GOALS:
+            def sort_by_goals(player):
+                return player.goals
 
-        sorted_players = sorted(
-            self._players,
-            reverse=True,
-            key=sort_by_points
-        )
+            sorted_players = sorted(
+                self._players,
+                reverse=True,
+                key=sort_by_goals
+            )
+        elif criteria == SortBy.ASSISTS:
+            def sort_by_assists(player):
+                return player.assists
+
+            sorted_players = sorted(
+                self._players,
+                reverse=True,
+                key=sort_by_assists
+            )
+        else:
+            
+            def sort_by_points(player):
+                return player.points
+
+            sorted_players = sorted(
+                self._players,
+                reverse=True,
+                key=sort_by_points
+            )
 
         result = []
         i = 0
-        while i <= how_many:
+        while i < how_many:
             result.append(sorted_players[i])
             i += 1
 
